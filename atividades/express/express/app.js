fddf387090713e429ba6bcc2c78f6e00aa061e15
luiz-ribeiro06 <luiz.ribeiro06@aluno.ifce.edu.br>
2025-10-06 10:31:45 -0300

    Update app.js

--- atividades/express/express/app.js
+++ atividades/express/express/app.js
@@ -2,30 +2,60 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/about", (req, res) => {
-  res.send("This is just a simple example app.");
+app.use((req, res, next) => {
+  console.log(`Acesso em: ${req.path} - ${new Date().toLocaleString()}`);
+  next();
 });
 
-app.post("/data", (req, res) => {
-  res.send("I'm not selling your data... probably.");
+function aboutMiddleware(req, res, next) {
+  console.log("Rota: /about");
+  next();
+}
+
+function dataMiddleware(req, res, next) {
+  console.log("Rota: /data");
+  next();
+}
+
+function signinMiddleware(req, res, next) {
+  console.log("Rota: /users/signin");
+  next();
+}
+
+function signupMiddleware(req, res, next) {
+  console.log("Rota: /users/signup");
+  next();
+}
+
+app.get("/about", aboutMiddleware, (req, res) => {
+  res.send("Página: About");
+});
+
+app.post("/data", dataMiddleware, (req, res) => {
+  res.send("Página: Data");
 });
 
 app.get("/users", (req, res) => {
-  res.send("I don't know what to say here. Users exist.");
+  res.redirect("/users/signup");
 });
 
-app.get("/users/:id", (req, res) => {
-  res.send("Here's your ID: idk");
-})
-
-app.get("users/signin", (req, res) => {
-  res.send("Sign In");
+app.get("/users/:userid", (req, res) => {
+  const userId = req.params.userid;
+  res.send(`Bem-vindo, usuário ${userId}!`);
 });
 
-app.get("users/signup", (req, res) => {
-  res.send("Sign Up");
+app.get("/users/signin", signinMiddleware, (req, res) => {
+  res.send("Página: Sign In");
+});
+
+app.get("/users/signup", signupMiddleware, (req, res) => {
+  res.send("Página: Sign Up");
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Página não encontrada</h1><a href="/">Voltar ao início</a>');
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}.`);
-})
+});

9baf320b29a40b1c25e8ec0fbccacf7e8213fb3f
luiz-ribeiro06 <luiz.ribeiro06@aluno.ifce.edu.br>
2025-10-06 10:33:37 -0300

    Update app.js

--- atividades/express/express/app.js
+++ atividades/express/express/app.js
@@ -17,16 +17,6 @@ function dataMiddleware(req, res, next) {
   next();
 }
 
-function signinMiddleware(req, res, next) {
-  console.log("Rota: /users/signin");
-  next();
-}
-
-function signupMiddleware(req, res, next) {
-  console.log("Rota: /users/signup");
-  next();
-}
-
 app.get("/about", aboutMiddleware, (req, res) => {
   res.send("Página: About");
 });
@@ -35,27 +25,13 @@ app.post("/data", dataMiddleware, (req, res) => {
   res.send("Página: Data");
 });
 
-app.get("/users", (req, res) => {
-  res.redirect("/users/signup");
-});
-
-app.get("/users/:userid", (req, res) => {
-  const userId = req.params.userid;
-  res.send(`Bem-vindo, usuário ${userId}!`);
-});
-
-app.get("/users/signin", signinMiddleware, (req, res) => {
-  res.send("Página: Sign In");
-});
-
-app.get("/users/signup", signupMiddleware, (req, res) => {
-  res.send("Página: Sign Up");
-});
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).send('<h1>404 - Página não encontrada</h1><a href="/">Voltar ao início</a>');
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}.`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
